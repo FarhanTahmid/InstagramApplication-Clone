51a6c84c570df3fe7295406eecb7f8ee664b0180
Farhan Tahmid <farhantahmid881@gmail.com>
2022-02-24 12:36:11 +0600

    Profile Activity Layout Creation

--- app/src/main/java/com/example/instagramapplicationclone/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/instagramapplicationclone/Profile/ProfileActivity.java
@@ -18,7 +18,7 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        //setupBottomNavView();
+        setupBottomNavView();
         toolBarSetup();
     }
 
