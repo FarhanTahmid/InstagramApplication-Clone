a5afe6499776fa3e03c66daac6afd2434b66e1cf
Farhan Tahmid <farhantahmid881@gmail.com>
2022-04-23 01:28:08 +0600

    UNIVERSAL IMAGE LOADER CONFIG

--- app/src/main/java/com/example/instagramapplicationclone/Profile/EditProfileFragment.java
+++ app/src/main/java/com/example/instagramapplicationclone/Profile/EditProfileFragment.java
@@ -1,20 +1,36 @@
 package com.example.instagramapplicationclone.Profile;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.instagramapplicationclone.R;
+import com.example.instagramapplicationclone.utilities.UniversalImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class EditProfileFragment extends Fragment {
-
+    private ImageView profileImage;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        View view= inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        profileImage=(ImageView) view.findViewById(R.id.profilePicture);
+        initImageLoader();
+        setProfileImage();
+        return view;
+    }
+    private void initImageLoader(){
+        UniversalImageLoader universalImageLoader= new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+    private void setProfileImage(){
+        Log.d("Set Profile Image","Setting Profile Image.");
+        String imgURL="www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconsdb.com%2Fgreen-icons%2Fandroid-6-icon.html&psig=AOvVaw1WTrvxXuq0s9-vUknkpjWw&ust=1650740324437000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNDYsoitqPcCFQAAAAAdAAAAABAD";
+        UniversalImageLoader.setImage(imgURL,profileImage,null,"https://");
     }
 }
--- app/src/main/java/com/example/instagramapplicationclone/utilities/UniversalImageLoader.java
+++ app/src/main/java/com/example/instagramapplicationclone/utilities/UniversalImageLoader.java
@@ -1,13 +1,20 @@
 package com.example.instagramapplicationclone.utilities;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.example.instagramapplicationclone.R;
 import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 public class UniversalImageLoader {
     private static final int defaultImage= R.drawable.ic_android;
@@ -31,5 +38,36 @@ public class UniversalImageLoader {
                 .diskCacheSize(100*1024*1024).build();
         return configuration;
     }
+    public static void setImage(String imgURL, ImageView image, final ProgressBar progressBar, String append){
+        ImageLoader imageLoader=ImageLoader.getInstance();
+        imageLoader.displayImage(append + imgURL, image, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                if (progressBar!=null){
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+            }
 
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                if (progressBar!=null){
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                if (progressBar!=null){
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+                if (progressBar!=null){
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
 }
